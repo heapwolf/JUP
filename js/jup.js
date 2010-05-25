2863434d5a433111d02aa329bee7203ca1296298
fernmicro <fernmicro@gmail.com>
2010-05-25 04:35:56 -0500

    bugfix, major update

--- js/jup.js
+++ js/jup.js
@@ -1,116 +1,56 @@
 var JUP = (typeof JUP != "undefined") ? JUP : (function() {
 
-    var Util = {
-	
-        markup: [],
-        attributes: [],
-        lastout: [],
+	var Util = {
 
-        adopt: function(o) {
-            var c = (Object.prototype.toString.call(o) == "[object Array]") ? [] : {};
+		translate: function (o, data) {
+
+            var c = [], atts = []; var count = 1;
             for (var i in o) {
+				count++
                 if (o[i] && typeof o[i] == "object") {
-                    c[i] = this.adopt(o[i]);
+
+					if(Object.prototype.toString.call(o[i]) != "[object Array]") {
+				        for(var attribute in o[i]) {
+				            atts.push([" ", attribute, "=\"", o[i][attribute], "\""].join(""));
+				        }
+						c[i] = "";
+						c[0] = [c[0], atts.join("")].join("");
+					}
+					else {
+						c[i] = this.translate(o[i], data);
+					}
                 }
                 else {
-                    c[i] = o[i];
+					
+					c[i] = o[i].replace(/\{\{([^\{\}]*)\}\}/g,
+		                function(str, r) {
+							try { return data[r]; } catch(ex) { return r; }
+		                }
+		            );
                 }
+
+				if(typeof c[0] == "string") {
+					c[0] = ["<", o[0], atts.join(""), ">"].join("");
+					c.push("</" + o[0] + ">");
+				}	
             }
-            return c;
-        },
 
-        subst: function(s, o) { // inspired by doug crockford's "supplant" method.
+			if(count-1 == o.length) {
+				return [c.join("")]
+			}
+		}
+	}
 
-            var count = -1;
-            return s.replace(/\{\{([^\{\}]*)\}\}/g,
-                function(str, r) {
-                    if(!isNaN(r)) {
-                        return o[r];
-                    }
-
-                    count++;
-                    return o[(Object.prototype.toString.call(o) == "[object Array]") ? count : r];
-                }
-            );
-        },
-
-        resolve: function(val) {
-
-            var tag = null;
-            var selfClosing = false;
-
-            for(var i=0; i < val.length; i++) {
-
-                if(typeof val === "string") { // this must be a value
-
-                    this.markup.push(val);
-                    break;
-                }
-
-                if(typeof val[i] === "string" && i === 0) { // this must be a tag, its in the first position
-
-                    switch(val[i].toLowerCase()) {
-                        case "area":
-                        case "base":
-                        case "basefont":
-                        case "br":
-                        case "hr":
-                        case "input":
-                        case "img":
-                        case "link":
-                        case "meta":
-                            selfClosing = true;
-                        break;
-                    }
-
-                    // check to see if this array has any objects in it (check for attributes).
-                    for(var j=i; j < val.length; j++) {
-
-                        if(!(Object.prototype.toString.call(val[j]) == "[object Array]") && typeof val[j] == "object") {
-
-                            var a = val[j];
-
-                            for (var v in a) {
-
-                                this.attributes.push([" ", v, "='", a[v], "'"].join(""));
-                            }
-                        }
-                    }
-
-                    var close = selfClosing ? "/" : "";
-
-                    if(this.attributes.length > 0) {
-                        this.markup.push(["<", val[i], this.attributes.join(""), close, ">"].join(""));
-                        this.attributes = [];
-                        tag = val[i].indexOf(" ") == -1 ? val[i] : val[i].substr(0, val[i].indexOf(" "));
-                        continue;
-                    }
-
-                    this.markup.push(["<", val[i], close, ">"].join(""));
-                    tag = val[i];
-                    continue;
-                }
-
-                this.resolve(val[i]); // this must be a child.
-
-                if(i == val.length-1 && tag !== null && !selfClosing) {
-                    this.markup.push(["</", tag, ">"].join("")); // close it!
-                }
-            }
-        }
-    };
-
-    return {
+	return {
 
         data: function(str) {
             return ["{{", str, "}}"].join("");
         },
+		html: function() {
 
-        html: function() {
-
-            var args = Array.prototype.slice.call(arguments),
-                structure = [],
-                data = {};
+		 	var args = Array.prototype.slice.call(arguments),
+				structure = [],
+				data = {};
 
             if(args.length == 2) {
                 structure = args[1];
@@ -126,23 +66,23 @@ var JUP = (typeof JUP != "undefined") ? JUP : (function() {
                 }
             }
 
-            if(data !== null && data.length) {
-                for(var i=0; i < data.length; i++) {
-                    Util.resolve(structure);
-                    Util.markup = [Util.subst(Util.markup.join(""), data[i])];
-                }
-            } else if(data !== null) {
-                Util.resolve(structure);
-                Util.markup = [Util.subst(Util.markup.join(""), data)];
-            } else {
-                Util.resolve(structure);
-            }
+			if(Object.prototype.toString.call(data) == "[object Array]") {
 
-            Util.lastout = (data !== null) ? Util.subst(Util.markup.join(""), data) : Util.markup.join("");
-            Util.markup = [];
-            Util.attributes = []; // reset;
+				var copystack = [];
 
-            return Util.lastout;
-        }
-    };
+				for(var i=0; i < data.length; i++) {
+					copystack.push(Util.translate(structure, data[i])[0]);
+				}
+				return copystack.join("");
+			} 
+			else if(data) {
+				
+				for(var i=0; i < data.length; i++) {
+					return Util.translate(args[2] ? structure : Util.translate(structure)[0], data[i])
+				}
+			}
+
+			return Util.translate(structure, data)[0];
+		}
+	}
 })();
